e3b4456cbe7d863d7d6ecbb015a3088f4d567b66
Rufus Derrick R <97310908+Rufus2002@users.noreply.github.com>
2024-10-31 13:03:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#%% packages
+#Import Necessary Packages
 import os
 import streamlit as st
 __import__('pysqlite3')
@@ -14,19 +14,18 @@ import groq
 from groq import Groq
 client = Groq(api_key=("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"))
 
-#%% data prep
-# Step 1: Data Preparation
-ipcc_report_file = "SampleContract-Shuttle.pdf"
-reader = PdfReader(ipcc_report_file)
-ipcc_texts = [page.extract_text().strip() for page in reader.pages]
+#Data Preparation
+Contract_project_file = "SampleContract-Shuttle.pdf"
+reader = PdfReader(Contract_project_file)
+contract_texts = [page.extract_text().strip() for page in reader.pages]
 
-# Step 2: Text Splitting
+
 char_splitter = RecursiveCharacterTextSplitter(
     separators=["\n\n", "\n", ". ", " ", ""],
     chunk_size=1000,
     chunk_overlap=0.2
 )
-texts_char_splitted = char_splitter.split_text('\n\n'.join(ipcc_texts))
+texts_char_splitted = char_splitter.split_text('\n\n'.join(contract_texts))
 
 token_splitter = SentenceTransformersTokenTextSplitter(
     chunk_overlap=0.2,
@@ -42,9 +41,9 @@ for text in texts_char_splitted:
         print(f"Error in text: {text} - {e}")
         continue
 
-# Step 3: Vector Database - ChromaDB
+#Vector Database
 chroma_client = chromadb.PersistentClient(path="db")
-chroma_collection = chroma_client.get_or_create_collection("ipcc_report_file")
+chroma_collection = chroma_client.get_or_create_collection("Contract_project_file")
 
 # Add documents to the collection
 ids = [str(i) for i in range(len(texts_token_splitted))]
@@ -53,7 +52,7 @@ chroma_collection.add(
     documents=texts_token_splitted
 )
 
-#%%
+#Model
 def rag(query, n_results=5):
     print("RAG")
     res = chroma_collection.query(query_texts=[query], n_results=n_results)
@@ -70,21 +69,21 @@ def rag(query, n_results=5):
     print(docs)
     return content, docs
 
-#%%
+#Header of the page
 st.header("Contract Clauses Chatbot")
 
 # text input field
 user_query = st.text_input(label="", help="Ask here to learn about Contract Management", placeholder="What do you want to know about the Project contract?")
-
 rag_response, raw_docs = rag(user_query)
 
-st.header("Raw Information")
+#Raw Information for better analysis
+"""st.header("Raw Information")
 print(len(raw_docs))
 st.text(f"Raw Response 0: {raw_docs[0]}")
 st.text(f"Raw Response 1: {raw_docs[1]}")
 st.text(f"Raw Response 2: {raw_docs[2]}")
 st.text(f"Raw Response 3: {raw_docs[3]}")
-st.text(f"Raw Response 4: {raw_docs[4]}")
+st.text(f"Raw Response 4: {raw_docs[4]}")"""
 
 
 st.header("RAG Response")
